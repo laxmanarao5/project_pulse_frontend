97721d2260940a0542161707b119b01ff4f4ecea
laxman <laxmanarao.b@westagilelabs.com>
2023-09-15 11:18:05 +0530

    github action push trigger 5

--- src/components/RootComponent.js
+++ src/components/RootComponent.js
@@ -6,7 +6,7 @@ function RootComponent() {
   return (
     <div>
         <div style={{minHeight:"80vh"}}>
-            <div className='fw-bold text-sucess'> Deployed from github actions trial 4 </div>
+            <div className='fw-bold text-sucess'> Deployed from github actions trial 5 </div>
             <Outlet/>
         </div>
         <Footer/>
